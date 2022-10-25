cf249882a6eb1ebfd30622da8dc781ee39116dff
Samidh Desai <samidhdesai@MacBook-Pro-5.local>
2022-10-25 17:11:26 -0500

    adds sidebar and state

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
-import { Ecommerce, Orders, Calendar, Employees, Stacked, Customers, Kanban, Area, Bar, Pie, Line, Financial, ColorMapping, Editor } from './pages'
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Customers, Kanban, Area, Bar, Pie, Line, Financial, ColorMapping, Editor } from './pages';
+import { useStateContext } from './contexts/ContextProvider';
 import './App.css';
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
   return (
     <div>
       <BrowserRouter>
